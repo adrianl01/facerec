6427ab362bc4c9df687da59228eb5f7563a08fb9
adrianl01 <ga.leiva.1601@gmail.com>
2025-10-09 00:47:58 -0300

    2nd commit

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBiyUWpBW8GIRylbQS6wkdMY_S85IZkqhM",
-  authDomain: "assessment-task-adrianl01.firebaseapp.com",
-  projectId: "assessment-task-adrianl01",
-  storageBucket: "assessment-task-adrianl01.firebasestorage.app",
-  messagingSenderId: "100965690533",
-  appId: "1:100965690533:web:ed5a336d70557a35b5b14c",
-  measurementId: "G-5FVESP3D8N",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
